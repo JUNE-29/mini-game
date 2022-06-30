c70db4c97763e5fa1f9016652abc632e7b58839d
JUNE-29 <jiyuni529@gmail.com>
2022-06-30 15:01:20 +0900

    [add] 10초 카운팅 기능 추가

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 const startBtn = document.querySelector('.start_btn');
 const stopBtn = document.querySelector('.stop_btn');
+const sec = document.querySelector('.second');
+
+let timeout;
+let seconds = 10;
+
+function CountTime() {
+   sec.innerText = `${seconds.toString().padStart(2,"0")}`;
+
+   seconds--;
+
+   if(seconds < 0) {
+      stopBtn.classList.add('hidden');
+      startBtn.classList.remove('hidden');
+      return;
+   }
+    timeout = setTimeout(CountTime, 1000);
+}
+
+function CountStop() {
+   clearTimeout(timeout);
+   seconds = 10;
+}
 
 startBtn.addEventListener('click', () => {
+   CountStop();
    startBtn.classList.add('hidden');
    stopBtn.classList.remove('hidden');
+   CountTime();
 });
+
+
 
