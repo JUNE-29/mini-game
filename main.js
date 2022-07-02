b9beb865020bf2d9d22a717d9b77f969431cec09
JUNE-29 <jiyuni529@gmail.com>
2022-07-02 21:31:39 +0900

    [add] 중지 버튼 클릭 시 리플레이 버튼 활성화 기능 추가

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const startBtn = document.querySelector('.start_btn');
 const stopBtn = document.querySelector('.stop_btn');
+const replayBtn = document.querySelector('.replay_btn');
+const replayBox = document.querySelector('.replay_box');
 
 const sec = document.querySelector('.second');
 
@@ -7,6 +9,17 @@ const bgSound = document.querySelector('.bg_sound');
 
 let timeout;
 let seconds = 10;
+
+
+function CountStop() {
+   clearTimeout(timeout);
+   seconds = 10;
+}
+
+function StopMusic() {
+   bgSound.pause();
+   bgSound.currentTime = 0;
+}
 
 function CountTime() {
    sec.innerText = `${seconds.toString().padStart(2,"0")}`;
@@ -16,23 +29,33 @@ function CountTime() {
    if(seconds < 0) {
       stopBtn.classList.add('hidden');
       startBtn.classList.remove('hidden');
-      bgSound.pause();
-      bgSound.currentTime = 0;
+      StopMusic();
       return;
    }
     timeout = setTimeout(CountTime, 1000);
 }
 
-function CountStop() {
-   clearTimeout(timeout);
-   seconds = 10;
-}
-
-startBtn.addEventListener('click', () => {
+function StartGame() {
    bgSound.play();
    CountStop();
    startBtn.classList.add('hidden');
    stopBtn.classList.remove('hidden');
    CountTime();
+}
+
+startBtn.addEventListener('click', () => {
+   StartGame();
 });
+
+stopBtn.addEventListener('click',()=>{
+   replayBox.classList.remove('hidden');
+   CountStop();
+   StopMusic();
+});
+
+replayBtn.addEventListener('click',()=>{
+   replayBox.classList.add('hidden');
+   StartGame();
+});
+
 
