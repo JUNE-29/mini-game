064ae291b55489a66402c24e9a575e5e390e0cbf
JUNE-29 <jiyuni529@gmail.com>
2022-07-04 16:07:09 +0900

    [add] 여러 수의 고양이 캐릭터 무작위 배치 기능 추가

--- main.js
+++ main.js
@@ -2,53 +2,56 @@ const startBtn = document.querySelector('.start_btn');
 const stopBtn = document.querySelector('.stop_btn');
 const replayBtn = document.querySelector('.replay_btn');
 const replayBox = document.querySelector('.replay_box');
-const cat = document.querySelector('.cat');
+const catsImages = document.querySelector('.cats_images');
 
 const sec = document.querySelector('.second');
 
 const bgSound = document.querySelector('.bg_sound');
 
+const numberOfCats = 10;
+
 let timeout;
 let seconds = 10;
 
-function AddHiddenClass(name) {
+function addHiddenClass(name) {
    name.classList.add('hidden');
 }
 
-function RemoveHiddenClass(name) {
+function removeHiddenClass(name) {
    name.classList.remove('hidden');
 }
 
-function CountStop() {
+function countStop() {
    clearTimeout(timeout);
    seconds = 10;
 }
 
-function StopMusic() {
+function stopMusic() {
    bgSound.pause();
    bgSound.currentTime = 0;
 }
 
-function CountTime() {
+function countTime() {
    sec.innerText = `${seconds.toString().padStart(2,"0")}`;
 
    seconds--;
 
    if(seconds < 0) {
-      AddHiddenClass(stopBtn);
-      RemoveHiddenClass(startBtn);
-      StopMusic();
+      addHiddenClass(stopBtn);
+      // addHiddenClass(cat);
+      removeHiddenClass(startBtn);
+      stopMusic();
       return;
    }
-   timeout = setTimeout(CountTime, 1000);
+   timeout = setTimeout(countTime, 1000);
 }
 
-function StartGame() {
+function startGame() {
    bgSound.play();
-   CountStop();
-   AddHiddenClass(startBtn);
-   RemoveHiddenClass(stopBtn);
-   CountTime();
+   countStop();
+   addHiddenClass(startBtn);
+   removeHiddenClass(stopBtn);
+   countTime();
 }
 
 function getRandomIntInclusive(min, max) {
@@ -57,32 +60,54 @@ function getRandomIntInclusive(min, max) {
    return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
-function RandomCoordinate(name) {
+function addCharacter(index) {
+   let number = index;
+   let character;
+
+   for (i = 0; number > i; i++) {
+      character = createCharacter();
+      catsImages.appendChild(character);
+   }
+   
+}
+
+function createCharacter() {
    let x = getRandomIntInclusive(40, 1400);
    let y = getRandomIntInclusive(230, 481);
 
-   name.style.left=`${x}px`;
-   name.style.top = `${y}px`;
+      const character = document.createElement('img');
+      character.setAttribute('class', 'cat');
+      character.setAttribute('src', 'static/img/cat.png');
+   
+      character.style.left=`${x}px`;
+      character.style.top = `${y}px`;
+      return character;
 }
 
+function removeChatacter(index) {
+   let number = index;
+   let character;
+
+   for (i = 0; number > i; i++) {
+      const cat = document.querySelector('.cat');
+      cat.remove();
+   }
+}
 
 startBtn.addEventListener('click', () => {
-   StartGame();
-   RemoveHiddenClass(cat);
-   RandomCoordinate(cat);
-
+   startGame();
+   addCharacter(numberOfCats);
 });
 
 stopBtn.addEventListener('click',()=>{
-   RemoveHiddenClass(replayBox);
-   AddHiddenClass(cat);
-   CountStop();
-   StopMusic();
+   removeHiddenClass(replayBox);
+   removeChatacter(numberOfCats);
+   countStop();
+   stopMusic();
 });
 
 replayBtn.addEventListener('click',()=>{
-   AddHiddenClass(replayBox);
-   RemoveHiddenClass(cat);
-   StartGame();
-   RandomCoordinate(cat);
+   addHiddenClass(replayBox);
+   addCharacter(numberOfCats);
+   startGame();
 });
