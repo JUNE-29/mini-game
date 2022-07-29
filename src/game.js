47def55f67de707205e8f1c08536f605aef484d7
JUNE-29 <jiyuni529@gmail.com>
2022-07-29 12:28:57 +0900

    [mod] main.js 수정, game.js 파일 생성, 클래스 생성

---
+++ src/game.js
@@ -0,0 +1,143 @@
+"use strict";
+import Field from './field.js';
+import * as sound from './sound.js';
+
+export default class Game {
+    constructor(gameDuration, catCount, alienCount) {
+        this.gameDuration = gameDuration;
+        this.catCount = catCount;
+        this.alienCount = alienCount;
+
+        this.startBtn = document.querySelector('.start_btn');
+        this.stopBtn = document.querySelector('.stop_btn');
+        this.startBtn.addEventListener('click', () => {
+            if (this.started) {
+                this.stop();
+            } else {
+                this.start();
+            }
+        });
+        
+        this.stopBtn.addEventListener('click',()=>{
+            this.stop();
+        });
+
+        this.gameTimer = document.querySelector('.game__timer');
+        this.gameScore = document.querySelector('.game__score');
+
+        this.gameField = new Field(catCount, alienCount);
+        this.gameField.setClickListener(this.onCharacterClick);
+
+        this.started = false;
+        this.score = 0;
+        this.timer = undefined; 
+    }
+
+    setGameStopListener(onGameStop) {
+        this.onGameStop = onGameStop;
+    }
+
+    start() {
+        this.started = true;
+        this.initGame();
+        this.showStopButton();
+        this.showTimerAndScore();
+        this.startGameTimer();
+        sound.playBackground();
+    }
+    
+    stop() {
+        this.started = false;
+        this.stopGameTimer();
+        this.hideStopButton();
+        sound.playAlert();
+        sound.stopBackground();
+        this.onGameStop && this.onGameStop('cancel');
+    }
+
+    finish(win) {
+        this.started = false;
+        this.hideStopButton();
+        if(win) {
+            sound.playWin();
+        } else {
+            sound.playAlien();
+        }
+        this.stopGameTimer();
+        sound.stopBackground();
+        this.onGameStop && this.onGameStop(win? 'win': 'lose');
+    }
+
+    onCharacterClick = (character) => {
+        if(!this.started) {
+            return;
+        }
+        if(character ==='cat') {
+            this.score++;
+            this.updateScoreBoard();
+            if(this.score === this.catCount) {
+                this.finish(true);
+            }
+        } else if(character === 'alien') {
+            this.finish(false);
+        }
+    }
+
+    showStopButton() {
+        this.stopBtn.style.visibility = 'visible';
+    }
+    
+    hideStopButton() {
+        this.stopBtn.style.visibility = 'hidden';
+    }
+    
+    showTimerAndScore() {
+        this.gameTimer.style.visibility = 'visible';
+        this.gameScore.style.visibility = 'visible';
+    }
+    
+    hideStartButton() {
+        this.startBtn.style.visibility = 'hidden';
+    }
+    
+    startGameTimer() {
+        let remainingTimeSec = this.gameDuration;
+        this.updateTimerText(remainingTimeSec);
+        this.timer =  setInterval(()=>{
+        if(remainingTimeSec <= 0) {
+                clearInterval(this.timer);
+                this.finish(this.catCount === this.score);
+                this.hideStopButton();
+                sound.stopBackground();
+                return;
+        }
+            this.updateTimerText(--remainingTimeSec);
+        }, 1000);
+    }
+    
+    stopGameTimer() {
+        clearInterval(this.timer);
+    }
+        
+    updateTimerText(time) {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        this.gameTimer.innerText =  `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+    }
+        
+    initGame() {
+        this.score = 0;
+        this.gameScore.innerText=`${this.catCount.toString().padStart(2,"0")}`;
+        this.gameField.init();
+        this.hideStartButton();
+    }
+        
+    updateScoreBoard() {
+        this.gameScore.innerText = (this.catCount - this.score).toString().padStart(2,"0");
+    }
+        
+        
+
+
+}
+
--- src/main.js
+++ src/main.js
@@ -1,142 +1,35 @@
 "use strict";
 
 import Popup from './popup.js';
-import Field from './field.js';
-import * as sound from './sound.js';
-
-const startBtn = document.querySelector('.start_btn');
-const stopBtn = document.querySelector('.stop_btn');
-
-const gameTimer = document.querySelector('.game__timer');
-const gameScore = document.querySelector('.game__score');
-
-const CAT_COUNT = 10;
-const ALIEN_COUNT = 7;
-const GAME_DURATION_SEC= 10;
-
-let started = false;
-let score = 0;
-let timer = undefined; 
+import Game from './game.js';
 
 const gameFinishBanner =  new Popup();
+const game = new Game(10, 10, 10);
+game.setGameStopListener((reason) => {
+   let message;
+   switch(reason) {
+      case 'cancel':
+         message = 'replay? 💥🔫';
+         break;
+      case 'win':
+         message = 'YOU WON!!😻';
+         break;
+      case 'lose':
+         message = 'YOU LOST!!😿';
+         break;
+      default:
+         throw new Error('not valid reasen');
+   }
+   gameFinishBanner.showWithText(message);
+});
+
 gameFinishBanner.setClickListener(()=>{
-   startGame();
+   game.start();
 })
 
-const gameField = new Field(CAT_COUNT, ALIEN_COUNT);
-gameField.setClickListener(onCharacterClick);
 
-function onCharacterClick(character) {
-   if(!started) {
-      return;
-   }
-   if(character ==='cat') {
-      score++;
-      updateScoreBoard();
-      if(score === CAT_COUNT) {
-         finishGame(true);
-      }
-   } else if(character === 'alien') {
-      finishGame(false);
-   }
-}
 
-startBtn.addEventListener('click', () => {
-   if (started) {
-      stopGame();
-   } else {
-      startGame();
-   }
-});
 
-stopBtn.addEventListener('click',()=>{
-   stopGame();
-});
-
-function startGame() {
-   started = true;
-   initGame();
-   showStopButton();
-   showTimerAndScore();
-   startGameTimer();
-   sound.playBackground();
-}
-
-function stopGame() {
-   started = false;
-   stopGameTimer();
-   hideStopButton();
-   sound.playAlert();
-   sound.stopBackground();
-   gameFinishBanner.showWithText('replay? 💥🔫');
-}
-
-function finishGame(win) {
-   started = false;
-   hideStopButton();
-   if(win) {
-      sound.playWin();
-   } else {
-      sound.playAlien();
-   }
-   stopGameTimer();
-   sound.stopBackground();
-   gameFinishBanner.showWithText(win? 'YOU WON!!😻' : 'YOU LOST!!😿');
-}
-
-function showStopButton() {
-   stopBtn.style.visibility = 'visible';
-}
-
-function hideStopButton() {
-   stopBtn.style.visibility = 'hidden';
-}
-
-function showTimerAndScore() {
-   gameTimer.style.visibility = 'visible';
-   gameScore.style.visibility = 'visible';
-}
-
-function hideStartButton() {
-   startBtn.style.visibility = 'hidden';
-}
-
-function startGameTimer() {
-   let remainingTimeSec = GAME_DURATION_SEC;
-   updateTimerText(remainingTimeSec);
-   timer =  setInterval(()=>{
-      if(remainingTimeSec <= 0) {
-         clearInterval(timer);
-         finishGame(CAT_COUNT === score);
-         hideStopButton();
-         sound.stopBackground();
-         return;
-      }
-      updateTimerText(--remainingTimeSec);
-   }, 1000);
-}
-
-function stopGameTimer() {
-   clearInterval(timer);
-}
-
-function updateTimerText(time) {
-   const minutes = Math.floor(time / 60);
-   const seconds = time % 60;
-   gameTimer.innerText =  `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
-   
-}
-
-function initGame() {
-   score = 0;
-   gameScore.innerText=`${CAT_COUNT.toString().padStart(2,"0")}`;
-   gameField.init();
-   hideStartButton();
-}
-
-function updateScoreBoard() {
-   gameScore.innerText = (CAT_COUNT - score).toString().padStart(2,"0");
-}
 
 
 
